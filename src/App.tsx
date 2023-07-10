acdcc037f6400a1b8796120802aa9fdeefca11cb
Izabelruseva <rusevaizabel@gmail.com>
2023-07-10 11:26:37 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,47 @@
-import * as React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Header from './components/header/Header';
-import Home from './components/Home';
-import About from './components/about/About';
-import HeaderSmall from './components/header/HeaderSmall';
-import CreateTrip from './components/trip/CreateTrip';
-import { Signup } from "./components/signup-1/Signup";
-import { RouterPathEnum } from './enums/RouterPathEnum';
-import Members from './components/member/Members';
-
+import * as React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./components/Home";
+import About from "./components/about/About";
+import HeaderSmall from "./components/header/HeaderSmall";
+import CreateTrip from "./components/trip/CreateTrip";
+import { RouterPathEnum } from "./enums/RouterPathEnum";
+import Members from "./components/member/Members";
 
 interface IState {
   isSmallScreen: boolean;
 }
 
 class App extends React.Component<{}, IState> {
-  constructor(props : any){
+  constructor(props: any) {
     super(props);
 
     this.state = { isSmallScreen: false };
-}
+  }
   componentDidMount() {
     window.addEventListener("resize", this.updateDimensions);
 
-    this.updateDimensions( null );
+    this.updateDimensions(null);
   }
   componentWillUnmount() {
     window.removeEventListener("resize", this.updateDimensions);
   }
 
   private updateDimensions = (e: any) => {
-    this.setState( { isSmallScreen: window.innerWidth < 500 } );
-  }
+    this.setState({ isSmallScreen: window.innerWidth < 500 });
+  };
 
   public render() {
     return (
       <BrowserRouter>
         <div>
-          { this.state.isSmallScreen ? <HeaderSmall /> : <Header /> }
+          {this.state.isSmallScreen ? <HeaderSmall /> : <Header />}
           <Switch>
-            <Route exact={true} path={RouterPathEnum.HOME} component={Home}/>
-            <Route path={RouterPathEnum.ABOUT} component={About}/>
-            <Route path={RouterPathEnum.MEMBER} component={Members}/>
-            <Route path={RouterPathEnum.CREATE_TRIP} component={CreateTrip}/>
+            <Route exact={true} path={RouterPathEnum.HOME} component={Home} />
+            <Route path={RouterPathEnum.ABOUT} component={About} />
+            <Route path={RouterPathEnum.MEMBER} component={Members} />
+            <Route path={RouterPathEnum.CREATE_TRIP} component={CreateTrip} />
             <Redirect to={RouterPathEnum.HOME} />
-            <Route path={RouterPathEnum.SIGNUP} component={Signup}/>
           </Switch>
         </div>
       </BrowserRouter>
